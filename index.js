e2d64c95f36d4b0e121f74b8f594e5687eab3318
Vitória Barbosa <vitoriabarbosa2474@gmail.com>
2025-03-03 17:23:40 -0300

    list all to-dos

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts');
+const { select, input, checkbox } = require('@inquirer/prompts');
 
 let meta = { value: "Tomar 3L de água por dia", checked: "false" };
 let metas = [meta];
@@ -12,6 +12,33 @@ const cadastrarMeta = async() => {
   };
 
   metas.push({ value: meta, checked: false });
+};
+
+const listarMeta = async() => {
+  const resposta = await checkbox({
+    message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa", 
+    choices: [...metas],
+    instructions: false 
+  });
+
+  if(resposta.length == 0) {
+    console.log("Nenhuma meta selecionada!");
+    return;
+  };
+
+  metas.forEach((m) => {
+    m.checked = false;
+  });
+
+  resposta.forEach((resposta) => {
+    const meta = metas.find((m) => {
+      return m.value == resposta;
+    });
+
+    meta.checked = true;
+  });
+
+  console.log("Meta(s) marcadas comoconcluída(s)!");
 };
 
 const start = async() => {
@@ -32,7 +59,7 @@ const start = async() => {
         console.log(metas);
         break;
       case "listar":
-        console.log("vamos listar");
+        await listarMeta();
         break;
       case "sair":
         console.log("Até mais!");
